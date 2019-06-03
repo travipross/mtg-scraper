e62fa813930bba7fdf01e61fc3b2981cab04e8f9
travipross <travis.prosser@resson.com>
2019-06-03 16:13:02 -0300

    reorganize code again

--- mtg_scraper_utils.py
+++ mtg_scraper_utils.py
@@ -1,4 +1,5 @@
 import re
+from requests_html import HTMLSession
 
 
 def encode_for_url_without_escapes(card_name):
@@ -19,3 +20,22 @@ def build_url_from_card_name(card_dict):
         set_url += ":Foil"
     url = "/".join(["https://www.mtggoldfish.com/price/", set_url, name_url])
     return url
+
+
+def scrape_and_update(collection):
+    html_session = HTMLSession()
+    for card in collection:
+        url = build_url_from_card_name(card)
+        print("Scraping: " + url)
+        r = html_session.get(url)
+        stats_text = r.html.find(".price-card-statistics-paper").pop().text
+        paper_price_text = r.html.find(".price-box.paper").pop().text
+
+        paper_price = float(re.findall("PAPER\n(.*)", paper_price_text).pop())
+        stats_match_pattern = ".*Daily Change\n(.+).*\nWeekly Change\n(.+).*\nHighest Price\n(.+).*\nLowest Price\n(.+).*"
+        stats_matches = re.findall(stats_match_pattern, stats_text).pop()
+        stats_dict = dict(zip(["daily_change", "weekly_change", "highest_price", "lowest_price"],
+                              map(float, list(stats_matches))))
+        card.update(stats_dict)
+        card.update({"current_price": paper_price})
+        card.update({"url": url})
--- test_mtg.py
+++ test_mtg.py
@@ -1,26 +1,9 @@
 import json
-import re
-from requests_html import HTMLSession
-from mtg_scraper_utils import build_url_from_card_name
+from mtg_scraper_utils import scrape_and_update
 
-html_session = HTMLSession()
 
 with open("my_collection.json", 'r') as f:
     collection = json.load(f)
 
-for card in collection:
-    url = build_url_from_card_name(card)
-    print("Scraping: " + url)
-    r = html_session.get(url)
-    stats_text = r.html.find(".price-card-statistics-paper").pop().text
-    paper_price_text = r.html.find(".price-box.paper").pop().text
-
-    paper_price = float(re.findall("PAPER\n(.*)", paper_price_text).pop())
-    stats_match_pattern = ".*Daily Change\n(.+).*\nWeekly Change\n(.+).*\nHighest Price\n(.+).*\nLowest Price\n(.+).*"
-    stats_matches = re.findall(stats_match_pattern, stats_text).pop()
-    stats_dict = dict(zip(["daily_change", "weekly_change", "highest_price", "lowest_price"], map(float, list(stats_matches))))
-    card.update(stats_dict)
-    card.update({"current_price": paper_price})
-    card.update({"url": url})
-
+scrape_and_update(collection)
 print(json.dumps(collection, indent=4, sort_keys=True))

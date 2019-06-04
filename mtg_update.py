c83e0bcdfe6e4cb90b89055bce000294e135f460
Travis Prosser <travisprosser@gmail.com>
2019-06-04 09:27:23 -0300

    make scraping optional

--- mtg_update.py
+++ mtg_update.py
@@ -6,9 +6,14 @@ import pandas as pd
 a = argparse.ArgumentParser()
 a.add_argument("-p", "--path-to-collection", help="Path to json file containing data", default="my_collection.json")
 a.add_argument("--overwrite", help="Overwrite file contents with new data (be sure to have a backup)", action='store_true', default=False)
+a.add_argument("--update", help="Update information from web", action='store_true', default=False)
 args = vars(a.parse_args())
+
+# get arguments
 path = args.get("path_to_collection", "my_collection.json")
 overwrite = args.get("overwrite")
+update_collection = args.get("update")
+
 
 # Open json file for updating
 try:
@@ -19,17 +24,17 @@ except FileNotFoundError as e:
     print("File not found: %s" % path)
     exit(1)
 
-# Modify imported dictionary with new data
-updated_list = scrape_and_update(collection)
+# Modify imported dictionary with new data if instructed to do so
+if update_collection:
+    updated_list = scrape_and_update(collection)
+    print("New data exists for %d cards" % updated_list.count(True))
 
-print("New data exists for %d cards" % updated_list.count(True))
-
-# Overwrite original file with new information
-if overwrite and updated_list.count(True) > 0:
-    with open(path, 'w') as f:
-        json.dump(collection, f, indent=4, sort_keys=True)
-else:
-    print(json.dumps(collection, indent=4, sort_keys=True))
+    # Overwrite original file with new information
+    if overwrite and updated_list.count(True) > 0:
+        with open(path, 'w') as f:
+            json.dump(collection, f, indent=4, sort_keys=True)
+    else:
+        print(json.dumps(collection, indent=4, sort_keys=True))
 
 # convert to pandas data frame for easier number crunchin'
 df = pd.DataFrame(collection)
@@ -44,8 +49,7 @@ for i in range(3):
                                         top3.set.iloc[i],
                                         (" (Foil)" if top3.foil.iloc[i] else "")
                                         ))
+print("\nTotal value of card collection: $%0.2f USD" % (df.current_price * df.quantity).sum())
 print("----------------------------------------")
-print("Total value of card collection: $%0.2f USD" % (df.current_price * df.quantity).sum())
+print("Number of cards documented: %d (%d unique)" % (df.quantity.sum(), df.name.count()))
 print("Most duplicates: %s = qty %d" % (df.name.loc[df.quantity.idxmax()], df.quantity.max()))
-
-# TODO: Reorganize to make scraping optional and just read from file

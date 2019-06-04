dcc0dac176547eebab4821f40f14164120e9cefd
Travis Prosser <travisprosser@gmail.com>
2019-06-04 09:41:55 -0300

    add --show option to print json to console

--- mtg_update.py
+++ mtg_update.py
@@ -7,12 +7,14 @@ a = argparse.ArgumentParser()
 a.add_argument("-p", "--path-to-collection", help="Path to json file containing data", default="my_collection.json")
 a.add_argument("--overwrite", help="Overwrite file contents with new data (be sure to have a backup)", action='store_true', default=False)
 a.add_argument("--update", help="Update information from web", action='store_true', default=False)
+a.add_argument("--show", help="Print json file to console", action='store_true', default=False)
 args = vars(a.parse_args())
 
 # get arguments
 path = args.get("path_to_collection", "my_collection.json")
 overwrite = args.get("overwrite")
 update_collection = args.get("update")
+show = args.get("show")
 
 
 # Open json file for updating
@@ -33,8 +35,10 @@ if update_collection:
     if overwrite and updated_list.count(True) > 0:
         with open(path, 'w') as f:
             json.dump(collection, f, indent=4, sort_keys=True)
-    else:
-        print(json.dumps(collection, indent=4, sort_keys=True))
+
+# Show json collection in console
+if show:
+    print(json.dumps(collection, indent=4, sort_keys=True))
 
 # convert to pandas data frame for easier number crunchin'
 df = pd.DataFrame(collection)

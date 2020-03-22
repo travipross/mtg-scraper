f4f6f010bbc8315a9895c25f8ae45c7372b41a3e
Travis Prosser <travisprosser@gmail.com>
2020-03-22 17:29:35 -0300

    add followers

--- mtg_scraper/app/main/routes.py
+++ mtg_scraper/app/main/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app.main import bp
+from app.models import User
 
 @bp.route('/')
 @bp.route('/index')
@@ -8,4 +9,9 @@ def index():
 
 @bp.route('/<username>')
 def user(username):
-    return "user page"
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
+
+@bp.route('/edit_profile')
+def edit_profile():
+    return 'edit profile page'
--- mtg_scraper/app/models.py
+++ mtg_scraper/app/models.py
@@ -1,6 +1,13 @@
 from app import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from hashlib import md5
+
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
 
 class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,12 +23,23 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String, index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    followed = db.relationship(
+        'User',
+        secondary=followers,
+        primaryjoin=(followers.c.followed_id == id),
+        secondaryjoin=(followers.c.follower_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic'
+    )
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
     
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)

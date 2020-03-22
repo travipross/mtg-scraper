aa550b629fb884e2333a00d1e582ab7c2d168e7f
Travis Prosser <travisprosser@gmail.com>
2020-03-22 15:45:29 -0300

    reorganize for blueprints

---
+++ .flaskenv
@@ -0,0 +1,1 @@
+FLASK_APP=mtg_scraper/mtg_scraper.py
--- mtg_scraper/app/__init__.py
+++ mtg_scraper/app/__init__.py
@@ -3,9 +3,24 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app,db)
 
-from app import routes, models
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp)
+
+    return app
+
+from app import models
